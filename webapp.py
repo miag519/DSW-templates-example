6125601c6363258cbf527efc4b304529efb7b033
miag519 <miag1984.5@gmail.com>
2024-03-15 13:57:51 -0700

    finished entire generator and added more lines

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
@@ -14,5 +14,38 @@ def render_page1():
 def render_page2():
     return render_template('page2.html')
     
+@app.route("/p3")
+def render_page3():
+    return render_template('page3.html')
+    
+@app.route("/response")  
+def render_response():
+    word = request.args["word"]
+    word1 = request.args["word2"]
+    word2 = request.args["word3"]
+    number = request.args["number"]
+    if len(word) > 3:
+     username = (word[0:3] + word1)
+    else:
+     username = (word + word1)
+    if len(word2) > 4:
+     usernames = (number + word2[0:4] + number)
+    else:
+     usernames = (word2 + number)
+    reverse = (word2[::-1] + word)
+    if len(word) > 6 and len(word1) > 2:
+     half = (word[2:6] + number + word1[0:2])
+    else:
+     half = (word + number)
+    if len(word2) > 5 and len(number) > 3: 
+     mixed = (number[0:1] + word2[2:5] + number[2:3])
+    else:
+     mixed = (number + word2[::-1] + number)
+    return render_template('page1.html', a = username, b = usernames, c = reverse, d = half, e = mixed)
+    
+
+
+
+    
 if __name__=="__main__":
-    app.run(debug=False)
+    app.run(debug=True)
